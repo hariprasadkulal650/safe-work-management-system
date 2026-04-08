692fbe95c10568751a96cbf015901e70c0911221
Ankit-chaubey1 <chaubeyankit927@gmail.com>
2026-04-08 12:00:41 +0530

    updatd logic for employee

--- employee-service/src/main/java/com/cts/employee_service/controller/EmployeeController.java
+++ employee-service/src/main/java/com/cts/employee_service/controller/EmployeeController.java
@@ -1,9 +1,9 @@
+
+
+
 package com.cts.employee_service.controller;
 
-import com.cts.employee_service.dto.EmployeeResponseDTO;
-import com.cts.employee_service.dto.HazardDTO;
-import com.cts.employee_service.dto.LoginRequest;
-import com.cts.employee_service.dto.TrainingDTO;
+import com.cts.employee_service.dto.*;
 import com.cts.employee_service.entities.*;
 import com.cts.employee_service.service.IEmployeeService;
 import jakarta.validation.Valid;
@@ -31,7 +31,7 @@ public class EmployeeController {
         return ResponseEntity.ok(employeeService.loginEmployee(loginRequest.getEmail(), loginRequest.getPassword()));
     }
 
-    @GetMapping("/getall")
+    @GetMapping
     public ResponseEntity<List<EmployeeResponseDTO>> getAll() {
         return ResponseEntity.ok(employeeService.getAllEmployees());
     }
@@ -39,23 +39,6 @@ public class EmployeeController {
     @GetMapping("/{id}")
     public ResponseEntity<EmployeeResponseDTO> getEmployee(@PathVariable long id) {
         return ResponseEntity.ok(employeeService.getEmployeeById(id));
-    }
-
-    // Hazard Routes (using DTOs)
-    @PostMapping("/hazard/report")
-    public ResponseEntity<HazardDTO> reportHazard(@Valid @RequestBody HazardDTO hazardDTO) {
-        return new ResponseEntity<>(employeeService.reportHazard(hazardDTO), HttpStatus.CREATED);
-    }
-
-    @GetMapping("/{id}/hazards")
-    public ResponseEntity<List<HazardDTO>> getHazards(@PathVariable long id) {
-        return ResponseEntity.ok(employeeService.getHazardsByEmployee(id));
-    }
-
-    // Training Routes
-    @GetMapping("/{id}/trainings")
-    public ResponseEntity<List<TrainingDTO>> getMyTrainings(@PathVariable long id) {
-        return ResponseEntity.ok(employeeService.getTrainingsByEmployee(id));
     }
 
     @GetMapping("/{id}/document")
